36a3a8bbce35f298a08b6d747974b2a9b7199358
Tanzeel Alam <tanzeelalam@gmail.com>
2023-03-04 23:08:19 +0530

    Added Mohammed Tanzeel Alam

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -5,7 +5,7 @@ package com.mycompany.app;
  */
 public class App {
 
-    private static final String MESSAGE = "Hello World! From Tanzeel";
+    private static final String MESSAGE = "Hello World! From Mohammed Tanzeel Alam";
 
     public App() {}
 
