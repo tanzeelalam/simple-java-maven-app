8dd1e9c8f220d84f1bd9b44cd136269267f2a050
tanzeelalam <tanzeelalam@gmail.com>
2023-03-05 00:04:11 +0530

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -5,7 +5,7 @@ package com.mycompany.app;
  */
 public class App {
 
-    private static final String MESSAGE = "Bismillah.";
+    private static final String MESSAGE = "Bismillah Bismillah";
 
     public App() {}
 
