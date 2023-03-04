dbba1f027968c96390e6310f730b904b5a4dc39d
Tanzeel Alam <tanzeelalam@gmail.com>
2023-03-04 23:42:33 +0530

    Alhamdulillah

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -5,7 +5,7 @@ package com.mycompany.app;
  */
 public class App {
 
-    private static final String MESSAGE = "Bismillah Allahu akbar";
+    private static final String MESSAGE = "Bismillah Allahu akbar alhamdulillah";
 
     public App() {}
 
